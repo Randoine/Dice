c44b77595b3f75950c0d649572b6dbbde786dbcf
Rav-Komputer\Rav <dudziak.rafal@gmail.com>
2016-12-22 19:45:45 +0100

    Pierwszy commit

---
+++ app/src/main/java/dice/rav/com/dice/MainActivity.java
@@ -0,0 +1,13 @@
+package dice.rav.com.dice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
