767824940e038661840c5c87f572bef77da15cff
Rav <dudziak.rafal@gmail.com>
2016-12-22 20:40:29 +0100

    Poprawny start

--- app/src/main/java/dice/rav/com/dice/MainActivity.java
+++ app/src/main/java/dice/rav/com/dice/MainActivity.java
@@ -10,8 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //blabla
-        System.out.println("safsf");
-        SYSTEM_HEALTH_SERVICE
+
     }
 }
