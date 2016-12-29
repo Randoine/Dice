acf88e59192dda6420083886519bbd053ddf1d87
Rav <dudziak.rafal@gmail.com>
2016-12-29 19:01:38 +0100

    Layout dodatkowe zmiany

--- app/src/main/java/dice/rav/com/dice/MainActivity.java
+++ app/src/main/java/dice/rav/com/dice/MainActivity.java
@@ -3,7 +3,11 @@ package dice.rav.com.dice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
